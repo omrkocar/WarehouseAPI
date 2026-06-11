f52726d92811538e70878e3a2008993b49da2286
Omer <kocartahir@gmail.com>
2026-06-11 12:10:02 -0400

    Initial setup for the db context

---
+++ WarehouseAPI/Data/WarehouseDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WarehouseAPI.Models.Entities;
+
+namespace WarehouseAPI.Data;
+
+public class WarehouseDbContext(DbContextOptions<WarehouseDbContext> options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+    
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+}
