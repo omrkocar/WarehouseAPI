c23ff91535e400a2b0a98a50ea5c87aced1412c0
Omer <kocartahir@gmail.com>
2026-06-11 12:15:42 -0400

    Convert the status enum to string in the db

--- WarehouseAPI/Data/WarehouseDbContext.cs
+++ WarehouseAPI/Data/WarehouseDbContext.cs
@@ -5,12 +5,16 @@ namespace WarehouseAPI.Data;
 
 public class WarehouseDbContext(DbContextOptions<WarehouseDbContext> options) : DbContext(options)
 {
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-    }
-    
     public DbSet<Product> Products { get; set; }
     public DbSet<Order> Orders { get; set; }
     public DbSet<OrderItem> OrderItems { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Order>()
+            .Property(o => o.Status)
+            .HasConversion<string>();
+    }
 }
