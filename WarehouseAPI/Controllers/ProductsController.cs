722732b9f608393ccaab490d5ae70b55136622bc
Omer <kocartahir@gmail.com>
2026-06-11 17:10:42 -0400

    Implemented Create, GetById and Delete methods

--- WarehouseAPI/Controllers/ProductsController.cs
+++ WarehouseAPI/Controllers/ProductsController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WarehouseAPI.Models.DTOs;
+using WarehouseAPI.Models.Entities;
 using WarehouseAPI.Repositories;
 
 namespace WarehouseAPI.Controllers;
@@ -12,5 +14,40 @@ public class ProductsController(IProductRepository productRepository) : Controll
     {
         var products = await productRepository.GetAllAsync();
         return Ok(products);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateProductDto dto)
+    {
+        var product = new Product
+        {
+            Name = dto.Name,
+            Description = dto.Description,
+            Price = dto.Price,
+            StockQuantity = dto.StockQuantity
+        };
+
+        var created = await productRepository.CreateAsync(product);
+        return CreatedAtAction(nameof(GetById), new { id = created.Id }, created);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var product = await productRepository.GetByIdAsync(id);
+        if (product == null)
+            return NotFound();
+
+        return NoContent();
+    }
+    
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        var product = await productRepository.DeleteAsync(id);
+        if (product == null)
+            return NotFound();
+        
+        return Ok(product);
     }
 }
---
+++ WarehouseAPI/Models/DTOs/CreateProductDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WarehouseAPI.Models.DTOs;
+
+public class CreateProductDto
+{
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public int StockQuantity { get; set; }
+}
