bf1ccf6c8408972ecfacded8c09066e091e9d8b0
Omer <kocartahir@gmail.com>
2026-06-11 16:51:29 -0400

    Implemented products controller and GetAll method

---
+++ WarehouseAPI/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WarehouseAPI.Repositories;
+
+namespace WarehouseAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController(IProductRepository productRepository) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var products = await productRepository.GetAllAsync();
+        return Ok(products);
+    }
+}
