1d20748a312360c8c22a1064a08e6ab0d4435329
Omer <kocartahir@gmail.com>
2026-06-11 12:56:16 -0400

    Set up SQLite

--- WarehouseAPI/Program.cs
+++ WarehouseAPI/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using WarehouseAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<WarehouseDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 builder.Services.AddOpenApi();
+
 
 var app = builder.Build();
 
