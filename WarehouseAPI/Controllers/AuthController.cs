aa98884177989fa972e60eba97ae1fef57e56541
Omer <kocartahir@gmail.com>
2026-06-12 16:05:02 -0400

    Implemented auth controller

---
+++ WarehouseAPI/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WarehouseAPI.Models.DTOs;
+
+namespace WarehouseAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController(IAuthService authService) : ControllerBase
+{
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterDto dto)
+    {
+        var result = await authService.RegisterAsync(dto);
+
+        if (!result.IsSuccess)
+            return BadRequest(result.Error);
+
+        return Ok(result.Value);
+    }
+}
