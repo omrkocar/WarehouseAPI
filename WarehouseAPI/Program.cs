8cd589a78bffb14b70e885a7a7b381e4962c3ada
Omer <kocartahir@gmail.com>
2026-06-12 13:01:01 -0400

    Added JsonStringEnumConverter

--- WarehouseAPI/Program.cs
+++ WarehouseAPI/Program.cs
@@ -4,7 +4,12 @@ using WarehouseAPI.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+    });
+
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<WarehouseDbContext>(options =>
