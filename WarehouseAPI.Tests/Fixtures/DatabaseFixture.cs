d6ae4f2b176db7289dc5dea13d88595770c9abc1
Omer <kocartahir@gmail.com>
2026-06-13 13:40:07 -0400

    Created DatabaseFixture that creates db context

---
+++ WarehouseAPI.Tests/Fixtures/DatabaseFixture.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using WarehouseAPI.Data;
+
+namespace WarehouseAPI.Tests.Fixtures;
+
+public class DatabaseFixture : IDisposable
+{
+    private readonly string dbPath = $"test_{Guid.NewGuid()}.db";
+    
+    public WarehouseDbContext CreateContext()
+    {
+        var options = new DbContextOptionsBuilder<WarehouseDbContext>()
+            .UseSqlite($"Data Source={dbPath}")
+            .Options;
+
+        var context = new WarehouseDbContext(options);
+        context.Database.EnsureCreated();
+        return context;
+    }
+
+    public void Dispose()
+    {
+        if (File.Exists(dbPath))
+            File.Delete(dbPath);
+    }
+}
